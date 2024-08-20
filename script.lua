2b33c8b8fedcf3457e7b270014a8eccc90e010f9
Rafael S.R <110256307+rsley@users.noreply.github.com>
2024-08-20 01:33:46 +0000

    Globalize creditsTab and settingsTab

--- script.lua
+++ script.lua
@@ -921,6 +921,9 @@ function Library:create(options)
 	creditsTab:credit{Name = "Abstract", Description = "UI Library Developer", Discord = "Abstract#8007", V3rmillion = "AbstractPoo"}
 	creditsTab:credit{Name = "Deity", Description = "UI Library Developer", Discord = "Deity#0228", V3rmillion = "0xDEITY"}
 	creditsTab:credit{Name = "Repository", Description = "UI Library", Github="https://github.com/AscendScripting/Mercury/blob/main/script.lua"}
+
+  getgenv().AC_CREDITSTAB = creditsTab
+  getgenv().AC_SETTINGSTAB = settingsTab
 	return mt
 end
 
